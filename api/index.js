81d08f4d7b9f95102f7725b932c893a4f24f738e
Ramnath <ramnath2544@gmail.com>
2026-04-03 17:00:01 +0530

    Create and run the Express server

--- api/index.js
+++ api/index.js
@@ -1,28 +1,25 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+
 dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  }),
-);
-
-app.get('/api/test', (req, res) => {
-  res.json({ message: 'SyncDoc API is running perfectly!' });
-});
-
-const PORT = 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}!`);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
--- api/models/User.js
+++ api/models/User.js
@@ -1,35 +1,36 @@
 import mongoose from 'mongoose';
 
+export const DEFAULT_PROFILE_PICTURE =
+  'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
+
 const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
       required: true,
       unique: true,
-      trim: true,
-      minlength: 3,
     },
     email: {
       type: String,
       required: true,
       unique: true,
-      lowercase: true,
-      trim: true,
     },
     password: {
       type: String,
       required: true,
-      minlength: 6,
     },
     profilePicture: {
       type: String,
-      default:
-        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+      default: DEFAULT_PROFILE_PICTURE,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
     },
   },
-  { timestamps: true },
+  { timestamps: true }
 );
 
-const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
