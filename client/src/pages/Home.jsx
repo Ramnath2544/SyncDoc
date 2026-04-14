6f02b3ce7f019a550fa5b3195ff7b3f61d969af2
Ramnath <ramnath2544@gmail.com>
2026-04-14 10:43:14 +0530

    Fix the bug

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className='min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900'>
       <h1 className='text-4xl font-bold text-gray-800 dark:text-white'>
-        Welcome to SyncDoc 🚀
+        Welcome to SyncDoc 
       </h1>
     </div>
   );
